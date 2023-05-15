3f0ad76b07e6be0e8a777dd3fce6d8ed5e232473
Eugene Polyakov <jenick12345@yandex.ru>
2023-05-15 15:48:19 +0300

    9th lesson

---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Technologies;
