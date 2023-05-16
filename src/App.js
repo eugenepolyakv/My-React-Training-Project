ce318e471892252333b6717655ce0bc92bf174d9
Eugene Polyakov <jenick12345@yandex.ru>
2023-05-16 16:43:43 +0300

    12th lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import Technologies from './Technologies';
-import Header from './Header';
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png"></img>
+            </header>
+            <nav className="nav">
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+            <div className="content">
+                <div className="imgOnAllWidth">
+                    <img src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg" />
+                </div>
+                <div>pfp + description</div>
+                <div>My posts</div>
+                <div>New post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
         </div>
     );
 };
