af85f7672246a3d91d947a2c210aef1e7779897d
Eugene Polyakov <jenick12345@yandex.ru>
2023-05-21 14:05:56 +0300

    22th lesson
    routes for user's dialogs have been added, route for dialogs' component has changed into /dialogs/* so that when opening a dialogue with a specific person, component has been shown anyway, even though url is another

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,7 +1,26 @@
 import classes from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
-    return <div>Dialogs</div>;
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.messages}>
+                <div>
+                    <NavLink to="/dialogs/1">Valera</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/dialogs/2">Denis</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/dialogs/3">Natasha</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/dialogs/4">Kirill</NavLink>
+                </div>
+            </div>
+            <div className={classes.view}></div>
+        </div>
+    );
 };
 
 export default Dialogs;
